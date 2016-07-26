2c5f44d20bc42a5400854b67ca0e104802be209f
Haoming Zhang <feicun1006@gmail.com>
2016-07-25 18:29:09 -0700

    Fix git clone problem of private repositry

--- build-process.sh
+++ build-process.sh
@@ -7,14 +7,14 @@ spawn mkdir tmp-clone
 
 # Clone repository
 spawn git clone -b $branch --single-branch $url ./tmp-clone
+# Deal with connect to SSH server with private key prompt
+# "Are you sure you want to continue connecting"
 expect "yes/no"
 send "yes\r"
-# expect "Enter passphrase for key" {
-    # send "\r"
-# }
+# Make sure git clone completed
 expect "Checking connectivity... done."
+# Another check to make sure git clone completed
 expect eof
-# expect eof # Wait for git clone complete
 
 # Build image
 spawn /root/execute.sh docker build -t $tag -f ./tmp-clone/Dockerfile ./tmp-clone
