450b513cb6bda801685ed7e626adabf30fb19325
Haoming Zhang <feicun1006@gmail.com>
2016-08-05 11:39:30 -0700

    Remove useless commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,30 +13,22 @@ mkdir /root/.ssh
 # Create public key file
 pubdest=/root/.ssh/id_rsa.pub
 touch "$pubdest"
-#if [ -f "$pubdest" ]
-#then
-#    echo "$public_key" > "$pubdest"
-#fi
 echo "$public_key" > "$pubdest"
 chmod 600 $pubdest
 
 # Create authorized key file
-authdest=/root/.ssh/authorized_keys
-touch "$authdest"
+#authdest=/root/.ssh/authorized_keys
+#touch "$authdest"
 #if [ -f "$authdest" ]
 #then
 #    echo "$public_key" > "$authdest"
 #fi
-echo "$public_key" > "$authdest"
-chmod 600 $authdest
+#echo "$public_key" > "$authdest"
+#chmod 600 $authdest
 
 # Create private key file
 privdest=/root/.ssh/id_rsa
 touch "$privdest"
-#if [ -f "$privdest" ]
-#then
-#    echo "$private_key" > "$privdest"
-#fi
 echo "$private_key" > "$privdest"
 chmod 600 $privdest
 
@@ -50,10 +42,10 @@ hostconfig='StrictHostKeyChecking no'
 echo "$host" > "$sshconfig"
 echo "$hostconfig" > "$sshconfig"
 
-mkdir tmp-clone
+mkdir tmp-gitclone
 
-git clone -b $branch --single-branch $url ./tmp-clone
+git clone -b $branch --single-branch $url ./tmp-gitclone 2>&1
 
-docker build -t $tag -f ./tmp-clone/Dockerfile ./tmp-clone
+docker build -t $tag -f ./tmp-gitclone/Dockerfile ./tmp-gitclone
 
 docker push $tag
