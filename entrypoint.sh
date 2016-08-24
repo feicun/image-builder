70b9917acd4fb9e971e5854544852187d512a916
Haoming Zhang <feicun1006@gmail.com>
2016-08-24 16:09:46 -0700

    Enable debug mode later, avoid record ssh keys in container log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-set -x
 
 tag=$1
 url=$2
@@ -15,16 +14,6 @@ pubdest=/root/.ssh/id_rsa.pub
 touch "$pubdest"
 echo "$public_key" > "$pubdest"
 chmod 600 $pubdest
-
-# Create authorized key file
-#authdest=/root/.ssh/authorized_keys
-#touch "$authdest"
-#if [ -f "$authdest" ]
-#then
-#    echo "$public_key" > "$authdest"
-#fi
-#echo "$public_key" > "$authdest"
-#chmod 600 $authdest
 
 # Create private key file
 privdest=/root/.ssh/id_rsa
@@ -44,10 +33,13 @@ echo "$hostconfig" > "$sshconfig"
 
 mkdir tmp-gitclone
 
+set -x
 # 2>&1 redirect git clone output to stdout
-git clone -b $branch --single-branch $url ./tmp-gitclone 2>&1
+# git clone -b $branch --single-branch $url ./tmp-gitclone 2>&1
+git clone -b $branch --single-branch $url ./tmp-gitclone
 
-docker build -t $tag -f ./tmp-gitclone/Dockerfile ./tmp-gitclone 2>&1
+# docker build -t $tag -f ./tmp-gitclone/Dockerfile ./tmp-gitclone 2>&1
+docker build -t $tag -f ./tmp-gitclone/Dockerfile ./tmp-gitclone
 
-docker push $tag 2>&1
-
+# docker push $tag 2>&1
+docker push $tag
